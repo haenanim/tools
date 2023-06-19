53c8c4246a683cbb1160d9ca32ea0b4b063f460c
haenanim121@gmail.com <haenanim121@gmail.com>
2023-06-19 18:10:43 +0900

    fix : 계산 로직 변경

--- src/components/Calc.tsx
+++ src/components/Calc.tsx
@@ -1,149 +1,135 @@
-import React, { SyntheticEvent, useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './Calc.css';
 
 export default function Calc() {
-  const [inputNum, setInputNum] = useState<string>('0');
-  const [waitInput, setWaitInput] = useState<boolean>(true);
-  const [resNum, setResNum] = useState<number>(0);
+  const [inputNum, setInputNum] = useState('0');
+  const [resNum, setResNum] = useState(0);
+  const [selectOperator, setSelectOperator] = useState('+');
+  const [readyNewInput, setReadyNewInput] = useState(true);
 
   function clickNumpad(e: any) {
-    if (waitInput) {
-      setInputNum(e.target.id);
-      setWaitInput(false);
+    const clickedNum = e.target.id;
+    if (readyNewInput) {
+      setInputNum(clickedNum);
+      setReadyNewInput(false);
     } else {
-      if (inputNum === '0') {
-        setInputNum(e.target.id);
-      } else {
-        setInputNum(inputNum + e.target.id);
-      }
+      setInputNum((prev) => prev + clickedNum);
     }
   }
-  function updateDisplay(result: number) {
+
+  function updateDisplay(result: any) {
     setInputNum(String(result));
   }
-  function clickOperator(operator: string) {
-    setWaitInput(true);
-    switch (operator) {
+
+  function clickOperator(operator: any) {
+    switch (selectOperator) {
       case '+':
-        setResNum((prev) => {
-          const result = prev + Number(inputNum);
-          updateDisplay(result);
-          return result;
-        });
+        handleEqual();
+        setSelectOperator('+');
+        if (inputNum !== '0' && inputNum && inputNum !== '-') {
+          setReadyNewInput(true);
+        }
+
         break;
       case '-':
+        handleEqual();
+        setSelectOperator('-');
+        if (readyNewInput) {
+          setReadyNewInput(false);
+          setInputNum('-');
+        } else if (!readyNewInput) {
+          setReadyNewInput(true);
+        }
         break;
       case '*':
+        setSelectOperator('*');
+        if (!readyNewInput) {
+        }
         break;
       case '/':
+        setSelectOperator('/');
+        break;
+      default:
         break;
     }
+    setSelectOperator(operator);
   }
+
+  function handleEqual() {
+    let result = 0;
+    switch (selectOperator) {
+      case '+':
+        result = resNum + Number(inputNum);
+        break;
+      case '-':
+        result = resNum - Number(inputNum);
+        break;
+      case '*':
+        result = resNum * Number(inputNum);
+        break;
+      case '/':
+        result = resNum / Number(inputNum);
+        break;
+      default:
+        result = resNum;
+    }
+    updateDisplay(result);
+    setResNum(result);
+  }
+
+  function handleClear() {
+    setInputNum('0');
+    setResNum(0);
+  }
+
   function makeNumpad() {
     const pad = [];
-    for (let i = 1; i <= 9; ++i) {
+    for (let i = 1; i <= 9; i++) {
       pad.push(
-        <div
-          id={String(i)}
-          onClick={(e) => {
-            clickNumpad(e);
-          }}
-          key={i}
-        >
+        <div id={String(i)} onClick={clickNumpad} key={i}>
           {i}
         </div>
       );
-      if (i === 3) {
+      if (i === 3 || i === 6 || i === 9) {
         pad.push(
           <div
-            id="+"
-            onClick={(e) => {
-              clickOperator('+');
-            }}
-            key="+"
+            id={i === 3 ? '+' : i === 6 ? '-' : '*'}
+            onClick={() => clickOperator(i === 3 ? '+' : i === 6 ? '-' : '*')}
+            key={i === 3 ? '+' : i === 6 ? '-' : '*'}
           >
-            +
-          </div>
-        );
-      }
-      if (i === 6) {
-        pad.push(
-          <div
-            id="-"
-            onClick={(e) => {
-              clickNumpad(e);
-            }}
-            key="-"
-          >
-            -
-          </div>
-        );
-      }
-      if (i === 9) {
-        pad.push(
-          <div
-            id="*"
-            onClick={(e) => {
-              clickNumpad(e);
-            }}
-            key="*"
-          >
-            *
+            {i === 3 ? '+' : i === 6 ? '-' : '*'}
           </div>
         );
       }
     }
     pad.push(
-      <div
-        id="C"
-        onClick={(e) => {
-          clickNumpad(e);
-        }}
-        key="C"
-      >
+      <div id="C" onClick={handleClear} key="C">
         C
       </div>
     );
     pad.push(
-      <div
-        id="0"
-        onClick={(e) => {
-          clickNumpad(e);
-        }}
-        key="0"
-      >
+      <div id="0" onClick={clickNumpad} key="0">
         0
       </div>
     );
     pad.push(
-      <div
-        id="E"
-        onClick={(e) => {
-          clickNumpad(e);
-        }}
-        key="E"
-      >
-        E
+      <div id="=" onClick={handleEqual} key="=">
+        =
       </div>
     );
     pad.push(
-      <div
-        id="/"
-        onClick={(e) => {
-          clickNumpad(e);
-        }}
-        key="/"
-      >
+      <div id="/" onClick={() => clickOperator('/')} key="/">
         /
       </div>
     );
 
     return pad;
   }
+
   return (
     <div>
       <div>
-        <div className="res-display"> {inputNum}</div>
+        <div className="res-display">{inputNum}</div>
         <div className="pad">
           <div className="num-pad">{makeNumpad()}</div>
         </div>
